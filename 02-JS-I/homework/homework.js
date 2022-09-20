fa0d864508cd57c0375e05a7326ca858fe934650
pablodeangelis <deangelis.pablo@gmail.com>
2022-09-20 17:04:58 -0300

    keep trying

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -105,7 +105,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  ( x % y);
 }
 
 function esPar(num) {
